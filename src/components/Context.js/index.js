4d3baf16543549214de21b4925b7f8c30692a3e0
Murat GOKDUMAN <drmuratgokduman@gmail.com>
2023-05-23 10:32:00 +0300

    react anasayfa v1.0.1

--- src/components/Context.js/index.js
+++ src/components/Context.js/index.js
@@ -3,22 +3,31 @@ import photo from "../../img/Image_edited.jpg";
 
 function Context() {
   return (
-    <div className="row contentArea d-flex align-items-center">
+    <div className="row contentArea d-flex align-items-center my-5">
       <div className="col-12 col-sm-6 photoArea">
-        <img src={photo} alt="" srcset="" className="photo w-100 p-5 rounded-pill" />
+        <img
+          src={photo}
+          alt=""
+          srcset=""
+          className="photo w-100 p-5 rounded-pill"
+        />
       </div>
       <div className="col-12 col-sm-6 text">
-        <h3 className="mb-3 ozlusoz"><q>Ben bir taşım, bu da benim yuvarlanışım.</q></h3>
-        <h4 className="mt-5 mb-3 ozlusoz text-center">
-          Ben <strong>Dr. Murat Gökduman</strong>
-        </h4>
+        <h3 className="mb-3 ozlusoz text-center">
+          <q>
+            Endişelenmeye gerek yok. Ben bir taşım, bu da benim yuvarlanışım.
+            Her taş nasıl yuvarlanıyorsa öyle işte
+          </q>
+        </h3>
+        <p className="yazar"><strong>Petra</strong> - <strong>Marianna Coppo</strong></p>
+        <h4 className="mt-5 mb-3 name text-center">Ben Dr. Murat Gökduman</h4>
         <p className="ben text-center">
-          İşyeri hekimiyim. Aynı zamanda fonksiyonel tıp yolcusuyum.
+          İşyeri hekimiyim. Aynı zamanda fonksiyonel tıp yolcusuyum. Amatör
+          fotoğrafçıyım. Fullstack web development ile ilgileniyorum.
         </p>
-        <p className="ben text-center">Amatör fotoğrafçıyım.</p>
-        <p className="ben text-center">Fullstack web development ile ilgileniyorum.</p>
         <p className="ben text-center">
-          En büyük tutkum yeni şeyler öğrenmek. Öğrenmekten büyük haz alıyorum. Bunun için okuyorum, izliyorum ve uyguluyorum.
+          En büyük tutkum yeni şeyler öğrenmek. Öğrenmekten büyük haz alıyorum.
+          Bunun için okuyorum, izliyorum ve uyguluyorum.
         </p>
       </div>
     </div>
